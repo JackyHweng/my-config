69a6b008292a361468d8862fc24aa1f1655a2aea
huangjiajie <huangjiajie@lizhi.fm>
2022-12-29 12:01:59 +0800

    fish dev directory

--- nvim/plugin/0_mason.rc.lua
+++ nvim/plugin/0_mason.rc.lua
@@ -3,10 +3,40 @@ if (not status) then return end
 local status2, lspconfig = pcall(require, "mason-lspconfig")
 if (not status2) then return end
 
-mason.setup({
+-- nvim-cmp supports additional completion capabilities, so broadcast that to servers
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
 
+
+local servers = {
+  -- clangd = {},
+  -- gopls = {},
+  pyright = {},
+  -- rust_analyzer = {},
+  tsserver = {},
+  tailwindcss = {},
+
+  sumneko_lua = {
+    Lua = {
+      workspace = { checkThirdParty = false },
+      telemetry = { enable = false },
+    },
+  },
+}
+
+
+mason.setup({
 })
 
+
 lspconfig.setup {
+  ensure_installed = vim.tbl_keys(servers),
+  ui = {
+    icons = {
+      package_installed = "✓",
+      package_pending = "➜",
+      package_uninstalled = "✗"
+    }
+  },
   automatic_installation = true
 }
